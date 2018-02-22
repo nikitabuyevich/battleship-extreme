d3447bfe6ab9310c3d47d48328b8ec4e269e606f
Nikita Buyevich <nikita.buyevich@gmail.com>
2018-02-22 13:32:26 -0600

    Update refactor block and free code

--- Assets/Project/Scripts/Player/Helper/PlayerCollisionHelper.cs
+++ Assets/Project/Scripts/Player/Helper/PlayerCollisionHelper.cs
@@ -14,7 +14,7 @@ public class PlayerCollisionHelper : MonoBehaviour
 			if (direction == Direction.North)
 			{
 				var northBlocked = Physics2D.Raycast(new Vector2(player.transform.position.x, player.transform.position.y), Vector2.up, 1f);
-				if (northBlocked.collider != null && (northBlocked.collider.gameObject.tag == "Block" || northBlocked.collider.gameObject.tag == "Player"))
+				if (northBlocked.collider != null && (SpaceIsBlocked(northBlocked.collider.gameObject.GetComponentsInParent<Transform>())))
 				{
 					return false;
 				}
@@ -22,7 +22,7 @@ public class PlayerCollisionHelper : MonoBehaviour
 			else if (direction == Direction.South)
 			{
 				var southBlocked = Physics2D.Raycast(new Vector2(player.transform.position.x, player.transform.position.y), Vector2.down, 1f);
-				if (southBlocked.collider != null && (southBlocked.collider.gameObject.tag == "Block" || southBlocked.collider.gameObject.tag == "Player"))
+				if (southBlocked.collider != null && (SpaceIsBlocked(southBlocked.collider.gameObject.GetComponentsInParent<Transform>())))
 				{
 					return false;
 				}
@@ -30,7 +30,7 @@ public class PlayerCollisionHelper : MonoBehaviour
 			else if (direction == Direction.West)
 			{
 				var westBlocked = Physics2D.Raycast(new Vector2(player.transform.position.x, player.transform.position.y), Vector2.left, 1f);
-				if (westBlocked.collider != null && (westBlocked.collider.gameObject.tag == "Block" || westBlocked.collider.gameObject.tag == "Player"))
+				if (westBlocked.collider != null && (SpaceIsBlocked(westBlocked.collider.gameObject.GetComponentsInParent<Transform>())))
 				{
 					return false;
 				}
@@ -39,13 +39,26 @@ public class PlayerCollisionHelper : MonoBehaviour
 			else
 			{
 				var eastBlocked = Physics2D.Raycast(new Vector2(player.transform.position.x, player.transform.position.y), Vector2.right, 1f);
-				if (eastBlocked.collider != null && (eastBlocked.collider.gameObject.tag == "Block" || eastBlocked.collider.gameObject.tag == "Player"))
+				if (eastBlocked.collider != null && (SpaceIsBlocked(eastBlocked.collider.gameObject.GetComponentsInParent<Transform>())))
 				{
 					return false;
 				}
 			}
 
 			return true;
+		}
+
+		return false;
+	}
+
+	private bool SpaceIsBlocked(Transform[] transforms)
+	{
+		foreach (var transform in transforms)
+		{
+			if (transform.tag == "Block")
+			{
+				return true;
+			}
 		}
 
 		return false;
