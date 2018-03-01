7732bf01c50074bba1ff9cffad00262c6628f6a9
Nikita Buyevich <nikita.buyevich@gmail.com>
2018-03-01 16:43:20 -0600

    Add proper camera

---
+++ Assets/Project/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameCamera : MonoBehaviour
+{
+
+	private ITurn _turn;
+
+	[Inject]
+	public void Construct(ITurn turn)
+	{
+		_turn = turn;
+	}
+
+	void LateUpdate()
+	{
+		transform.position = new Vector3(
+			_turn.CurrentPlayer().transform.position.x,
+			_turn.CurrentPlayer().transform.position.y,
+			transform.position.z
+		);
+	}
+}
