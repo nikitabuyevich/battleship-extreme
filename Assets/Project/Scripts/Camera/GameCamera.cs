447d88e412d1acde08626df517daf8f6b0465c21
Nikita Buyevich <nikita.buyevich@gmail.com>
2018-03-01 22:44:00 -0600

    Add follow camera boundaries

--- Assets/Project/Scripts/Camera/GameCamera.cs
+++ Assets/Project/Scripts/Camera/GameCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using Zenject;
 
 public class GameCamera : MonoBehaviour
@@ -12,14 +13,33 @@ public class GameCamera : MonoBehaviour
 	public void Construct(ITurn turn)
 	{
 		_turn = turn;
+		bounds = GetBounds();
 	}
+
+	private BoundsInt bounds;
+
+	// Basically in order to get a 16:9 perfect pixel ratio we need this
+	private float magicNumber = 0.26f;
 
 	void LateUpdate()
 	{
 		transform.position = new Vector3(
-			_turn.CurrentPlayer().transform.position.x,
-			_turn.CurrentPlayer().transform.position.y,
-			transform.position.z
-		);
+			Mathf.Clamp(_turn.CurrentPlayer().transform.position.x, 4.76f, bounds.xMax + 1 + magicNumber),
+			Mathf.Clamp(_turn.CurrentPlayer().transform.position.y, 2f, bounds.yMax),
+			transform.position.z);
+	}
+
+	private BoundsInt GetBounds()
+	{
+		var tilemaps = GameObject.Find("Floor").GetComponentsInChildren<Tilemap>();
+		foreach (var tilemap in tilemaps)
+		{
+			if (tilemap.name == "Walls")
+			{
+				return tilemap.cellBounds;
+			}
+		}
+
+		return new BoundsInt();
 	}
 }
