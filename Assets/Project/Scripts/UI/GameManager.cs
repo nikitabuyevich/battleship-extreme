560f1e69d9aa84445aa985577a86f1cb47e87099
Nikita Buyevich <nikita.buyevich@gmail.com>
2018-04-03 19:42:13 -0500

    Update map

---
+++ Assets/Project/Scripts/UI/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public void LoadStartMenu()
+	{
+		SceneManager.LoadScene("Start Menu");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void BackToMainMenu()
+	{
+		SceneManager.LoadScene("Main Menu");
+	}
+
+}
---
+++ Assets/Project/Scripts/UI/NumberOfPlayersSlider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberOfPlayersSlider : MonoBehaviour
+{
+	public Text numberOfPlayers;
+
+	public void SelectNumberOfPlayers(float slider)
+	{
+		StaticVariables.numberOfPlayers = (int) slider;
+		numberOfPlayers.text = "NUMBER OF PLAYERS: " + slider;
+	}
+
+}
